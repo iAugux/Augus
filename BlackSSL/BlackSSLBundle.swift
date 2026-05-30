78f33dc006f9b8deceea62992a9b8dd24e52b81d
iAugus <iAugux@gmail.com>
2026-05-30 22:09:32 +0800

    feat: add BlackSSL widget extension and app group entitlements

---
+++ BlackSSL/BlackSSLBundle.swift
@@ -0,0 +1,12 @@
+// Created by Augus on 5/30/26
+// Copyright © 2026 Augus <iAugux@gmail.com>
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BlackSSLBundle: WidgetBundle {
+    var body: some Widget {
+        BlackSSL()
+    }
+}
