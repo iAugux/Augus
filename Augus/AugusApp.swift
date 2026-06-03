de0d43e624b2a7cf52f36ea586aa3cc50c706269
iAugus <iAugux@gmail.com>
2026-06-03 22:15:52 +0800

    Disable multiple windows on macOS by replacing WindowGroup with Window

--- Augus/AugusApp.swift
+++ Augus/AugusApp.swift
@@ -6,11 +6,15 @@ import SwiftUI
 @main
 struct AugusApp: App {
     var body: some Scene {
+#if os(macOS)
+        Window("Augus", id: "main") {
+            ContentView()
+                .frame(minWidth: 393, idealWidth: 393, minHeight: 700, idealHeight: 700)
+        }
+#else
         WindowGroup {
             ContentView()
-#if os(macOS)
-                .frame(minWidth: 393, idealWidth: 393, minHeight: 700, idealHeight: 700)
-#endif
         }
+#endif
     }
 }
